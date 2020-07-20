b7dfd12436543b5ec78d89b0dcd100a7a39f5215
Raumo0 <dharapko@gmail.com>
2020-07-21 00:13:39 +0300

    Added QR generator template

--- src/main/java/com/biteacon/ExplorerBot.java
+++ src/main/java/com/biteacon/ExplorerBot.java
@@ -1,10 +1,14 @@
 package com.biteacon;
 
 import com.biteacon.constants.BotConfig;
+import net.glxn.qrgen.javase.QRCode;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
 
 public class ExplorerBot extends TelegramLongPollingBot {
     @Override
@@ -15,9 +19,13 @@ public class ExplorerBot extends TelegramLongPollingBot {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
             String response_message_text = CommandFilter.getInstance().matchCommands(message_text);
-            SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(response_message_text);
+
+            // get QR file from text using defaults
+            File file = QRCode.from(message_text).file();
+            SendPhoto message = new SendPhoto().setChatId(chat_id).setPhoto(file).setCaption(response_message_text);
+//            SendMessage message = new SendMessage() // Create a message object object
+//                    .setChatId(chat_id)
+//                    .setText(response_message_text);
             try {
                 execute(message); // Sending our message object to user
             } catch (TelegramApiException e) {
