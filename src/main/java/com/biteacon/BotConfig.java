cb5694fdb22fe47e5642ea023df3aeb06d4bb921
Raumo0 <dharapko@gmail.com>
2020-07-20 13:42:00 +0300

    Telegram api test. (echo bot)

---
+++ src/main/java/com/biteacon/BotConfig.java
@@ -0,0 +1,6 @@
+package com.biteacon;
+
+public class BotConfig {
+    public static final String EXPLORER_TOKEN = "<token>";
+    public static final String EXPLORER_USER = "Biteacon_bot";
+}
---
+++ src/main/java/com/biteacon/ExplorerBot.java
@@ -0,0 +1,37 @@
+package com.biteacon;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ExplorerBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BotConfig.EXPLORER_USER;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConfig.EXPLORER_TOKEN;
+    }
+}
