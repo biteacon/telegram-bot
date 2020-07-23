dca8e20f1d4f8b8e0b06fa04a561d10c13e624f3
Raumo0 <dharapko@gmail.com>
2020-07-24 02:29:02 +0300

    Added hello command constants

--- src/main/java/com/biteacon/constants/Commands.java
+++ src/main/java/com/biteacon/constants/Commands.java
@@ -5,6 +5,8 @@ public class Commands {
     public static final String HELP2 = "help";
     public static final String START = "/start";
     public static final String START2 = "start";
+    public static final String HELLO = "/hello";
+    public static final String HELLO2 = "hello";
     public static final String FAVORITES = "/favorites";
     public static final String FAVORITES2 = "favorites";
     public static final String LAST_BLOCK = "/lastblock";
