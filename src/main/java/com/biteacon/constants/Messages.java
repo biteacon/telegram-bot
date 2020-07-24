0e1502694de0fe428b524e905dcdee6180520f78
Raumo0 <dharapko@gmail.com>
2020-07-24 12:08:50 +0300

    "Help", "hello" and "start" message has been updated

--- src/main/java/com/biteacon/constants/Messages.java
+++ src/main/java/com/biteacon/constants/Messages.java
@@ -3,9 +3,27 @@ package com.biteacon.constants;
 public class Messages {
     public static final String NOT_FOUND = "not found!";
     public static final String ERROR = "error";
-    public static final String START_COMMAND = "Hello from start command!";
-    public static final String HELLO_COMMAND = "Hello from hello command!";
-    public static final String HELP_COMMAND = "Hello from help command!";
+    public static final String START_COMMAND = "<b>What can this bot do?</b>\n\n" +
+            "Welcome! This is the <b>Biteacon Bot</b>, it was created specially for <b>UTON HACK 2.0</b>. " +
+            "Here you can find the data of <b>LikeLib</b> blockchain and... get a chance for an invite to biteacon.xyz!\uD83D\uDE04\n\n" +
+//            "I can help you find any data on the <b>LikeLib</b> blockchain. " +
+            "If you're new to the <b>Biteacon</b>, please see the manual on our channel @biteacon " +
+            "and visit our biteacon.xyz business intelligence web app for <b>LikeLib</b> blockchain analytics.\n" +
+            "\uD83E\uDD73\uD83E\uDD29\uD83D\uDE0E\nDon't stop just type:\n\n" +
+            "/help - to read about specific command\n\n" +
+            "Contact @dharapko if you have questions or feedback about the Bot.";
+    public static final String HELLO_COMMAND = "Hello my friend! I send you rays of good!";
+    public static final String HELP_COMMAND = "You can control me by sending these commands:\n\n" +
+            "/help - to read about specific command\n\n" +
+            "/lastblock - to get the last block\n\n" +
+            "/accounts - to get a list of accounts\n\n" +
+            "/blocks - to get a list of blocks\n\n" +
+            "/transactions - to get a list of transactions\n\n" +
+            "/contracts - o get a list of smart contracts\n\n" +
+            "/hi - say hello to everyone!\n\n" +
+            "/start - what can this bot do?\n" +
+            "------------------------------\n" +
+            "/inviteme - get a chance for an invite to biteacon.xyz!";
 
     private Messages() {};
 }
